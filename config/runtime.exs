00c3b2b9d0c593200098a017af6b4c0dcb4a0d26
zili <neodejack@gmail.com>
2025-10-17 15:27:59 +0800

    use json for config file

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,37 +1,24 @@
 import Config
 
 if config_env() == :prod or :dev do
-  config_path = Path.expand("~/.rr/config.toml")
+  config_json = Path.expand("~/.rr/config.json") |> File.read!() |> JSON.decode!()
 
-  config_map =
-    case Toml.decode_file(config_path, []) do
-      {:ok, contents} ->
-        contents
+  parse_func = fn config ->
+    case config do
+      %{
+        "rancher_hostname" => _,
+        "rancher_token" => _
+      } ->
+        config
 
-      {:error, reason} ->
-        message = if is_binary(reason), do: reason, else: inspect(reason)
-        raise "Unable to parse #{config_path}: #{message}"
+      true ->
+        raise "config file not right"
     end
+  end
 
-  rancher_config =
-    case config_map do
-      %{"rancher" => section} -> section
-      _ -> raise "Missing [rancher] section in #{config_path}"
-    end
-
-  hostname =
-    case rancher_config do
-      %{"hostname" => value} -> value
-      _ -> raise "Missing hostname entry under [rancher] in #{config_path}"
-    end
-
-  token =
-    case rancher_config do
-      %{"token" => value} -> value
-      _ -> raise "Missing token entry under [rancher] in #{config_path}"
-    end
+  config = parse_func.(config_json)
 
   config :rr, RR,
-    rancher_hostname: hostname,
-    rancher_token: token
+    rancher_hostname: config["rancher_hostname"],
+    rancher_token: config["rancher_token"]
 end
