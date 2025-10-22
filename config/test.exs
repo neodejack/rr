16aeb54ea61f0c9220b40448a773447391c62ca1
zili <neodejack@gmail.com>
2025-10-22 16:30:37 +0800

    revamp config
    config state only always and only stay in the config.json file

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,5 @@
 import Config
 
-config :rr, RR,
-  enabled: false,
-  rancher_hostname: System.get_env("RANCHER_HOSTNAME"),
-  rancher_token: System.get_env("RANCHER_TOKEN"),
-  config_path: Path.expand("~/.rr/test/config.json")
+System.put_env("RR_HOME", "~/.rr/test")
+
+config :rr, RR, enabled: false
