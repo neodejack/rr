ac0c124b591974b5a59facb4114dce295e34a3d5
zili <neodejack@gmail.com>
2025-11-28 18:08:58 +0800

    release v0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RR.MixProject do
   def project do
     [
       app: :rr,
-      version: "0.1.3",
+      version: "0.1.5",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
