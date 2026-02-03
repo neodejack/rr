02e5478cf0e51a9df0a9ac21a564c821cf43a4f2
zili <neodejack@gmail.com>
2026-02-03 16:23:53 +0800

    refactor token validity check and warn on close to expiring rancher token

--- lib/external/rancher_http_client.ex
+++ lib/external/rancher_http_client.ex
@@ -1,11 +1,8 @@
 defmodule External.RancherHttpClient do
   @moduledoc false
   alias __MODULE__
+  alias RR.Config.Auth
   alias RR.KubeConfig
-
-  @callback auth_validation(%RR.Config.Auth{}) :: :ok | :error
-
-  def auth_validation(auth), do: impl().auth_validation(auth)
 
   @callback get_clusters() :: {:ok, [dynamic()]} | {:error, String.t()}
 
@@ -14,6 +11,10 @@ defmodule External.RancherHttpClient do
   @callback get_kubeconfig!(%KubeConfig{}) :: %KubeConfig{}
 
   def get_kubeconfig!(kubeconfig), do: impl().get_kubeconfig!(kubeconfig)
+
+  @callback get_token_info(%Auth{}) :: any()
+
+  def get_token_info(auth), do: impl().get_token_info(auth)
 
   defp impl, do: Module.concat([RancherHttpClient, Application.get_env(:rr, :external_bound, Impl)])
 end
